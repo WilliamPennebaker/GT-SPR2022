a865ceb15479d23739b2e0776248ebb370c23e5d
William Pennebaker <42590061+WilliamPennebaker@users.noreply.github.com>
2022-04-08 16:01:17 -0400

    Initial commit

---
+++ src/app/Controller.java
@@ -0,0 +1,31 @@
+//=============================================================================
+//PROGRAMMER1: Samantha
+// PANTHER ID1: Your panther ID
+// CLASS: Your class: COP5507
+//
+//PROGRAMMER2: William Pennebaker
+// PANTHER ID2: 6154734
+// CLASS: Your class: COP5507
+//
+// SEMESTER: Spring 2022
+// CLASSTIME: COP5507 Online
+//
+// Project: Description of project
+// DUE: Sunday, April 17, 11:59pm
+//
+// CERTIFICATION: I certify that this work is my own and that
+// none of it is the work of any other person.
+//=============================================================================
+package app;
+
+public class Controller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
